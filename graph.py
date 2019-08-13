fixed vertical gradient sign problem

graph.py
@@ -116,13 +116,16 @@ def graphSurfaces(surfaces,quantindex,contour=False,profiles=None,deepestindex=N
             mapy.fillcontinents(color='coral',lake_color='aqua')
             mapy.drawcoastlines()
             x,y = mapy(surfaces[i]["lons"],surfaces[i]["lats"])
+            d = np.asarray(surfaces[i]["data"][quantindex])
+            x = np.asarray(x)
+            y = np.asarray(y)
             #Plot the surface 
             if contour:
                 plt.tricontourf(x,y,np.asarray(surfaces[i]["data"][quantindex]),cmap="plasma")
             else:
-                plt.scatter(x,y,c=np.asarray(surfaces[i]["data"][quantindex]),cmap="plasma")
-                m = np.nanmedian(np.asarray(surfaces[i]["data"][quantindex]))
-                s = np.nanstd(np.asarray(surfaces[i]["data"][quantindex]))
+                plt.scatter(x,y,c=d,cmap="plasma")
+                m = np.nanmedian(d)
+                s = np.nanstd(d)
                 print("################")
                 plt.clim(m-2*s,m+2*s)
                 #plt.clim(i-400,i+400)

inverttools.py
@@ -813,7 +813,7 @@ def coupledInvert(surfaces,reflevel,neighbors,distances,debug=False,single=False
                 #######PVROW
                 betarow,betavals, crow = constructBetaRow(surfaces,k,distances,s,columnindexs)
                 betavals = np.asarray(betavals)
-                n = np.linalg.norm(np.concatenate((np.asarray(betavals),kpv[:1])))
+                n = np.linalg.norm(np.concatenate((np.asarray(betavals),kpv[[True,False,False]])))
 
                 #n = np.linalg.norm(betavals)
                 Apsi.append(np.asarray(betarow)/n)
@@ -825,7 +825,7 @@ def coupledInvert(surfaces,reflevel,neighbors,distances,debug=False,single=False
                 Akvb.append(Akvbrow)
 
                 Akhrow = [0]*(int(k/200))
-                Akhrow[(int(k/200)-1)] = kpv[2]/n
+                Akhrow[int(k/200-1)] = kpv[2]/n
                 Akh.append(Akhrow)
 
 
@@ -838,7 +838,7 @@ def coupledInvert(surfaces,reflevel,neighbors,distances,debug=False,single=False
                 ##make rows that can fit it 
                 salrow, salvals, crow = constructSalRow(surfaces,k,distances,s,columnindexs)
                 salvals = np.asarray(salvals)
-                n = np.linalg.norm(np.concatenate((np.asarray(salvals),ks[:1])))
+                n = np.linalg.norm(np.concatenate((np.asarray(salvals),ks[[True,False,False]])))
                 #n = np.linalg.norm(salvals)
                 Apsi.append(np.asarray(salrow)/n)
                 ##im a rascal and this is a shorthad way of converting the NS to an index :P
@@ -846,7 +846,7 @@ def coupledInvert(surfaces,reflevel,neighbors,distances,debug=False,single=False
                 Akvbrow[columnindexs[0]]=ks[1]/n
 
                 Akhrow = [0]*(int(k/200))
-                Akhrow[(int(k/200)-1)] = ks[2]/n
+                Akhrow[int(k/200-1)] = ks[2]/n
 
                 Akvb.append(Akvbrow)
                 Akh.append(Akhrow)
@@ -889,8 +889,9 @@ def coupledInvert(surfaces,reflevel,neighbors,distances,debug=False,single=False
     print("#############")
     c = np.matrix.transpose(np.asarray(c))
     us =  np.matrix.transpose(np.asarray(us))
-    j,[VT, D, U] = SVDdecomp(A,n_elements=int(A.shape[1]-7))
+    j,[VT, D, U] = SVDdecomp(A,n_elements=int(A.shape[1]))
     prime = np.matmul(j,c)
+    print(prime[-1])
     #graphError(A,np.concatenate((us,[0]*(len(us)+19))),prime)
     graphError(A,np.concatenate((us,[0]*(A.shape[1]-len(us)))),prime)
     graphKs(prime,m)
@@ -988,17 +989,22 @@ def rectangularize(a,l):
 def concat(argv):
     out = []
     for j in range(len(argv[0])):
-        row = []
+        row = np.array([])
         for b in argv:
-            row = np.concatenate((row,np.asarray(b[j])))
-        out.append(np.asarray(row))
+            row = np.concatenate((row,b[j]))
+        out.append(row)
     return np.asarray(out)
 
 def combineAs(maxlengths,*argv):
     new = []
     for i in range(len(argv)):
-        new.append(rectangularize(argv[i],maxlengths[i]))
-    return concat(np.asarray(new))
+        x  = np.asarray(rectangularize(argv[i],maxlengths[i]))
+        print(x.shape)
+        if np.all(x == 0, axis=0).any():
+            print("removing 0 columns")
+        x = x[:,~np.all(x == 0, axis=0)]
+        new.append(x)
+    return concat(new)
 
 def graphKs(prime,m):
     kvb = prime[m:2*m]

nstools.py
@@ -257,9 +257,10 @@ def addDataToSurfaces(profiles,surfaces,stdevs,debug=True):
                     t,s = p.atPres(surfaces[k]["data"]["pres"][l])
                 monthcount[p.time.month]=monthcount[p.time.month]+1
                 if pv and pv<0:
+                    pv=0
                     monthnegativecount[p.time.month]=monthnegativecount[p.time.month]+1
                     negativecount +=1 
-                if not pv:
+                if type(pv) == type(None):
                     pv = np.nan
                 if ~np.isnan(t) and ~np.isnan(s) and ~np.isnan(pv) and p and pv != np.Inf:
                     tempSurf["lons"].append(surfaces[k]["lons"][l])
@@ -565,8 +566,8 @@ def fillOutEmptyFields(surfaces):
 
 
 def vertGrad(out,data,depths,k,above,center,below,attr,outattr,factor=1):
-    dattr = data[depths[k-1]]["data"][attr][above]-data[depths[k+1]]["data"][attr][below]
-    dz = data[depths[k-1]]["data"]["pres"][above]-data[depths[k+1]]["data"]["pres"][below]
+    dattr = data[depths[k+1]]["data"][attr][below]-data[depths[k-1]]["data"][attr][above]
+    dz = data[depths[k+1]]["data"]["pres"][below]-data[depths[k-1]]["data"]["pres"][above]
     out[depths[k]]["data"][outattr][center] = factor * dattr/dz
     return out
 