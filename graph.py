x curvature and first derivatives correct

graph.py
@@ -140,7 +140,8 @@ def graphSurfacesComparison(surfaces,overlay,quantindex,contour=False,profiles=N
 ## if you really want you can supply a profiles object and a deepest index to display a point
 ## controls to save, graph or maximize
 def graphSurfaces(surfaces,quantindex,contour=False,profiles=None,deepestindex=None,\
-        show=True,maximize=True,savepath=None,idlabels=False,colorlimit=True,region="arctic"):
+        show=True,maximize=True,savepath=None,idlabels=False,\
+        colorlimit=True,region="arctic",select=[-np.inf,np.inf]):
     quanttitlehash = {"pres":"Pressure Dbar","t":"Temperature C","s":"Salinity PSU","pv":"PV",\
                      "u":"relative U","v":"relative V","psi":"ISOPYCNAL STREAMFUNCTION","hx":"Neutral Gradient X",\
                     "hy":"Neutral Gradient Y","curl":"Curl","drdt":"Northward Velocity",\
@@ -156,7 +157,8 @@ def graphSurfaces(surfaces,quantindex,contour=False,profiles=None,deepestindex=N
             print(e)
         writeInfoFile(savepath)
     for i in list(surfaces.keys()):
-        if quantindex in surfaces[i]["data"].keys() and \
+        if abs(i)>select[0] and abs(i) <select[1]\
+            and quantindex in surfaces[i]["data"].keys() and \
                 len(surfaces[i]["lons"])>3 and\
                 len(surfaces[i]["data"][quantindex])>3:
             fig,ax,mapy = mapSetup(surfaces,region=region)
@@ -824,9 +826,9 @@ def distanceHist(distances):
         plt.hist(distances[k].values())
         plt.show()
 
-def saveAllQuants(surfaces,savepath,region="arctic"):
+def saveAllQuants(surfaces,savepath,region="arctic",select=[-np.inf,np.inf]):
     for d in surfaces[list(surfaces.keys())[0]]["data"].keys():
-        graphSurfaces(surfaces,d,show=False,savepath=savepath,region=region)
+        graphSurfaces(surfaces,d,show=False,savepath=savepath,region=region,select=select)
 
         
 

interptools.py
@@ -267,8 +267,8 @@ def trueDistanceLookup(surface,neighbors):
         for edge in itertools.combinations(square,2):
             p = tuple(sorted(edge))
             if p not in lookup.keys():
-                #lookup[p] = geodesic((surface["lats"][p[0]],surface["lons"][p[0]]),(surface["lats"][p[1]],surface["lons"][p[1]])).m
-                latdist = abs(surface["lats"][p[0]] - surface["lats"][p[1]])*111.0*1000.0
-                londist = abs(surface["lons"][p[0]] - surface["lons"][p[1]]) *np.cos(np.deg2rad(surface["lats"][p[1]]))*111.0*1000.0
-                lookup[p] = latdist+londist 
+                lookup[p] = geodesic((surface["lats"][p[0]],surface["lons"][p[0]]),(surface["lats"][p[1]],surface["lons"][p[1]])).m
+                #latdist = abs(surface["lats"][p[0]] - surface["lats"][p[1]])*111.0*1000.0
+                #londist = abs(surface["lons"][p[0]] - surface["lons"][p[1]]) *np.cos(np.deg2rad(surface["lats"][p[1]]))*111.0*1000.0
+                #lookup[p] = latdist+londist 
     return lookup

nepbctdextract.py
@@ -125,7 +125,7 @@ def floatExtract(fname):
 
 
 
-def nepbCTDExtractInterpSurfaces(fname):
+def nepbCTDExtractInterpSurfaces(fname,calcDeriv = False):
     ctddata = sio.loadmat(fname)
     ##note: any x,y gradients wont be equivalent because of grid,
     ##			advisable to look at total gradient probably
@@ -146,13 +146,20 @@ def nepbCTDExtractInterpSurfaces(fname):
     surfaces = {}
 
     for field in ctddata.keys():
-        if field in quantmap.keys():
+        if field in list(quantmap.keys())+["F_Ssmooth","F_CTsmooth"]:
             if ctddata[field].shape == (20, 35, 30):
                 ctddata[field] = np.transpose(ctddata[field],(2,0,1))
+            if ctddata[field].shape == (41, 71, 30):
+                ctddata[field] = np.transpose(ctddata[field],(2,0,1))
+
     for k in Bar("surface").iter(range(len(ns))):
         tempSurf = nstools.emptySurface()
         for j in quantmap.values():
             tempSurf["data"][j]=[]
+        tempSurf["data"]["dsdx"]=[]
+        tempSurf["data"]["dsdy"]=[]
+        tempSurf["data"]["dtdx"]=[]
+        tempSurf["data"]["dtdy"]=[]
 
         for j in range(len(latlist)):
             for l in range(len(lonlist)):
@@ -161,7 +168,47 @@ def nepbCTDExtractInterpSurfaces(fname):
                 tempSurf["ids"].append(j*30+l)
                 for field in ctddata.keys():
                     if field in quantmap.keys():
-                        tempSurf["data"][quantmap[field]].append(ctddata[field][k][j][l])
+                        if calcDeriv and (field not in ["CTx","CTy","Sx","Sy"]):
+                            tempSurf["data"][quantmap[field]].append(ctddata[field][k][j][l])
+                        elif not calcDeriv:
+                            tempSurf["data"][quantmap[field]].append(ctddata[field][k][j][l])
+
+                    if calcDeriv and field == "F_Ssmooth":
+
+                        if j == len(latlist)-1 or l == len(lonlist)-1: 
+                            ydist = ((latlist[j] - latlist[j-1])/2.0)*111.0*1000.0
+                            xdist = ydist *np.cos(np.deg2rad(latlist[j]+1))
+                        else: 
+                            ydist = ((latlist[j+1] - latlist[j])/2.0)*111.0*1000.0
+                            xdist =ydist * np.cos(np.deg2rad(latlist[j]+1))
+
+                        dx = ctddata[field][k][j*2][l*2+1] -  ctddata[field][k][j*2][l*2]
+                        dx += ctddata[field][k][j*2+1][l*2+1] -  ctddata[field][k][j*2+1][l*2]
+                        dy = ctddata[field][k][j*2+1][l*2] -  ctddata[field][k][j*2][l*2]
+                        dy += ctddata[field][k][j*2+1][l*2+1] -  ctddata[field][k][j*2][l*2+1]
+
+                        tempSurf["data"]["dsdx"].append(dx/(2*xdist))
+                        tempSurf["data"]["dsdy"].append(dy/(ydist))
+
+
+                    if calcDeriv and field == "F_CTsmooth":
+
+                        if j == len(latlist)-1 or l == len(lonlist)-1: 
+                            ydist = ((latlist[j] - latlist[j-1])/2.0)*111.0*1000.0
+                            xdist = ydist *np.cos(np.deg2rad(latlist[j]+1))
+                        else: 
+                            ydist = ((latlist[j+1] - latlist[j])/2.0)*111.0*1000.0
+                            xdist =ydist * np.cos(np.deg2rad(latlist[j]+1))
+
+                        dx = ctddata[field][k][j*2][l*2+1] -  ctddata[field][k][j*2][l*2]
+                        dx += ctddata[field][k][j*2+1][l*2+1] -  ctddata[field][k][j*2+1][l*2]
+                        dy = ctddata[field][k][j*2+1][l*2] -  ctddata[field][k][j*2][l*2]
+                        dy += ctddata[field][k][j*2+1][l*2+1] -  ctddata[field][k][j*2][l*2+1]
+
+                        tempSurf["data"]["dtdx"].append(dx/(2*xdist))
+                        tempSurf["data"]["dtdy"].append(dy/(ydist))
+
+
 
         surfaces[ns[k][0]] = tempSurf
 